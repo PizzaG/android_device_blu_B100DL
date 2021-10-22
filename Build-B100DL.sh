0d049fb3f81f8429dafbe0858d21cd1957f4ca49
PizzaG <49663808+PizzaG@users.noreply.github.com>
2021-10-22 11:14:30 -0500

    Fix Build Script

--- Build-B100DL.sh
+++ Build-B100DL.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
 make clean
-
-make clean
 . build/envsetup.sh
 lunch omni_B100DL-eng
 mka recoveryimage
-
-echo " Recovery Should Be Built"
-echo ""
-read
 
 echo " Recovery Should Be Built"
 echo ""
